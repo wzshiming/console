4b804a48676e1ec8ea4a2ff38fcfe38370ff826a
wzshiming <wzshiming@foxmail.com>
2018-06-17 01:35:48 +0800

    fixes

--- shell/session_shell.go
+++ shell/session_shell.go
@@ -3,29 +3,37 @@ package session_shell
 import (
 	"fmt"
 	"io"
+	"os"
 	"os/exec"
 	"strconv"
 	"unsafe"
 
+	"github.com/kr/pty"
 	"github.com/wzshiming/console"
 )
 
 type SessionsShell struct {
-	sessions map[string]*exec.Cmd
+	sessions map[string]*os.File
 }
 
 var _ = (*SessionsShell)(nil)
 
 func NewShellSessions(host string) (console.Sessions, error) {
 	return &SessionsShell{
-		sessions: map[string]*exec.Cmd{},
+		sessions: map[string]*os.File{},
 	}, nil
 }
 
 func (d *SessionsShell) CreateExec(req *console.ReqCreateExec) (*console.RespCreateExec, error) {
-	cli := exec.Command(req.Cmd)
-	id := "0x" + strconv.FormatUint(uint64(uintptr(unsafe.Pointer(cli))), 16)
-	d.sessions[id] = cli
+	sh := exec.Command(req.Cmd)
+	id := "0x" + strconv.FormatUint(uint64(uintptr(unsafe.Pointer(sh))), 16)
+	// Start the command with a pty.
+	ptmx, err := pty.Start(sh)
+	if err != nil {
+		return nil, err
+	}
+
+	d.sessions[id] = ptmx
 	return &console.RespCreateExec{
 		EId: id,
 	}, nil
@@ -38,16 +46,14 @@ func (d *SessionsShell) StartExec(eid string, ws io.ReadWriter) error {
 	}
 	defer func() {
 		delete(d.sessions, eid)
+		cli.Close()
 	}()
 
-	cli.Stdin = ws
-	cli.Stdout = ws
-	cli.Stderr = ws
-
-	return cli.Run()
+	go io.Copy(cli, ws)
+	io.Copy(ws, cli)
+	return nil
 }
 
 func (d *SessionsShell) ResizeExecTTY(req *console.ReqResizeExecTTY) error {
-
 	return nil
 }
