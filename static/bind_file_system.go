d9832457c7d448a27e8e0aab7c529f73e89dc3e9
wzshiming <wzshiming@foxmail.com>
2019-05-07 17:04:49 +0800

    update static

--- static/bind_file_system.go
+++ static/bind_file_system.go
@@ -1,80 +1,16 @@
-//go:generate go-bindata -o static_binddata.go -pkg static -ignore '^\.' ./web/...
+//go:generate go-bindata -o static_binddata.go --pkg static --prefix web web/...
 
 package static
 
 import (
-	"bytes"
-	"io"
 	"net/http"
-	"os"
-	"path"
-	"strings"
+
+	"github.com/wzshiming/go-bindata/fs"
 )
 
-type File struct {
-	io.Reader
-	io.Seeker
-	ReaddirFunc func(count int) ([]os.FileInfo, error)
-	StatFunc    func() (os.FileInfo, error)
-}
-
-func (f File) Close() error {
-	return nil
-}
-
-func (f File) Readdir(count int) ([]os.FileInfo, error) {
-	return f.ReaddirFunc(count)
-}
-
-func (f File) Stat() (os.FileInfo, error) {
-	return f.StatFunc()
-}
-
-type FileSystem struct {
-	Dir  string
-	Root string
-}
-
-func NewFileSystem() *FileSystem {
-	return &FileSystem{
-		Dir:  "web",
-		Root: "index.html",
+func NewFileSystem() http.FileSystem {
+	return &fs.AssetFS{
+		Asset: Asset,
+		Index: "index.html",
 	}
-}
-
-func (f *FileSystem) Open(name string) (http.File, error) {
-	if strings.HasSuffix(name, "/") {
-		name = path.Join(f.Dir, name, f.Root)
-	} else {
-		name = path.Join(f.Dir, name)
-	}
-
-	d, err := Asset(name)
-	if err != nil {
-		return nil, err
-	}
-	reader := bytes.NewReader(d)
-
-	return File{
-		reader,
-		reader,
-		func(count int) ([]os.FileInfo, error) {
-			fi := []os.FileInfo{}
-			ps, err := AssetDir(path.Dir(name))
-			if err != nil {
-				return nil, err
-			}
-			for i := 0; i != count; i++ {
-				ai, err := AssetInfo(ps[i])
-				if err != nil {
-					return nil, err
-				}
-				fi = append(fi, ai)
-			}
-			return fi, nil
-		},
-		func() (os.FileInfo, error) {
-			return AssetInfo(name)
-		},
-	}, nil
 }
